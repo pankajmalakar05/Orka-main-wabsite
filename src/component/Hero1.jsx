062d31115830030e0708a2990402b533f7a8c4b4
pankaj malakar <mrpankajmalakar@gmail.com>
2026-03-14 19:09:59 +0530

    update code

--- src/component/Hero1.jsx
+++ src/component/Hero1.jsx
@@ -56,23 +56,66 @@
 // export default Hero1;
 
 
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { motion } from "framer-motion";
-// import video from "../assets/Orka-images/Frame 5.jpg";
- import image5 from "../assets/Orka-images/Frame 5.jpg";
+import { Link } from "react-router-dom";
+import heroBg from "../assets/Orka-images/herobgimage1.jpg";
+import waxImg from "../assets/Orka-images/aloveras7.jpg";
+
+const wax = {
+  id: 1,
+  name: "Aloe Vera Liposoluble Wax",
+  price: "₹1350/-",
+  mrp: "₹20%",
+  image: waxImg,
+};
 
 const Hero1 = () => {
-
   return (
-    <section className="w-full bg-black flex justify-center overflow-hidden pt-18 md:pt-0">
+    <section className="w-full relative">
+      {/* Background Hero Image */}
       <motion.img
-        src={image5}
-        alt="Frame 5"
+        src={heroBg}
+        alt="Hero Background"
         initial={{ scale: 1.1, opacity: 0 }}
         animate={{ scale: 1, opacity: 1 }}
         transition={{ duration: 1.4 }}
-        className="w-full object-cover"
+        className="w-full h-[120vh] object-cover"
       />
+
+      {/* Left Side Card */}
+      <div className="absolute top-[75%] left-4 md:left-45 transform -translate-y-1/2">
+        <Link to={`/product/${wax.id}`}>
+          <motion.div
+            initial={{ x: -100, opacity: 0 }} // slide in from left
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1 }}
+            className="bg-white shadow-lg rounded-xl p-4 md:p-2 w-52 md:w-55 cursor-pointer hover:scale-105 transition-transform"
+          >
+            {/* Product Image */}
+            <img
+              src={wax.image}
+              alt={wax.name}
+              className="w-full h-40 md:h-43 object-cover rounded-lg mb-0"
+            />
+
+            {/* Product Name */}
+            <h3 className="text-[#3e2723] font-semibold text-base md:text-sm mb-2">
+              {wax.name}
+            </h3>
+
+            {/* Price & MRP */}
+            <div className="flex items-center gap-2">
+              <span className="text-sm md:text-xl font-bold text-[#3e2723]">
+                {wax.price}
+              </span>
+              <span className="text-sm md:text-base text-gray-400 line-through">
+                {wax.mrp}
+              </span>
+            </div>
+          </motion.div>
+        </Link>
+      </div>
     </section>
   );
 };
